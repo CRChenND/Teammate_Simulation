66fe7d36e0ad07d59e43e5152a2f4a3ae90fd386
charles-c-chiang <cchiang3@nd.edu>
2024-04-19 11:17:22 -0400

    material ui example

--- Frontend/my-app/src/components/main/main.js
+++ Frontend/my-app/src/components/main/main.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 const Main = () => {
+
+    const navigate = useNavigate();
+    const profileNav = () => {
+        navigate("/profile");
+      };
+
+
     return (
         <div>
-            Hello World
+            <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+            <TextField id="outlined-basic" label="Hello World" variant="outlined" />
+            <Button variant="contained" onClick={profileNav} className="btn btn-secondary me-2" style={{ marginRight: '5px' }}>
+              Profile
+            </Button>
         </div>
     )
 }
