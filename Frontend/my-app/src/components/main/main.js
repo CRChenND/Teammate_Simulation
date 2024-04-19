bfbe9c623ba99c5f03952524887553be369908a1
charles-c-chiang <cchiang3@nd.edu>
2024-04-19 11:47:48 -0400

    basic changes

--- Frontend/my-app/src/components/main/main.js
+++ Frontend/my-app/src/components/main/main.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 const Main = () => {
 
@@ -14,11 +17,15 @@ const Main = () => {
     return (
         <div>
             <meta name="viewport" content="initial-scale=1, width=device-width" />
+            <Box display="flex" justifyItems="center" padding="12px">
+                    <TextField id="outlined-basic" label="Hello World" variant="outlined" fullWidth="True"/>
 
-            <TextField id="outlined-basic" label="Hello World" variant="outlined" />
-            <Button variant="contained" onClick={profileNav} className="btn btn-secondary me-2" style={{ marginRight: '5px' }}>
-              Profile
-            </Button>
+
+                <Button variant="contained" onClick={profileNav} className="btn btn-secondary me-2" style={{ marginLeft: '5px' }}>
+                Generate
+                </Button>
+            </Box>
+
         </div>
     )
 }
