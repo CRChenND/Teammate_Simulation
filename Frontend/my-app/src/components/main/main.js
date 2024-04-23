79c752080f8da32fd21cd6bc7e67a952b936b3f3
charles-c-chiang <cchiang3@nd.edu>
2024-04-23 12:14:48 -0400

    added fetch, not working

--- Frontend/my-app/src/components/main/main.js
+++ Frontend/my-app/src/components/main/main.js
@@ -6,23 +6,57 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
+
 const Main = () => {
 
+    const url = "127.0.0.1:5000"
     const navigate = useNavigate();
     const profileNav = () => {
         navigate("/profile");
       };
 
+    const [textInput, setTextInput] = useState("");
+
+    const handleChange = (e) => {
+        setTextInput(e.target.value)
+        console.log(textInput)
+    };
+
+    const onSubmit = () => { 
+        return (
+            fetch("/generate_profile", {
+                method: 'POST',
+                headers:{
+                    'type': 'application/json',
+                },
+                body: JSON.stringify(textInput)
+          }).then(response => response.json())
+          .then(data => console.log(data)
+          
+        )
+    );
+        
+    }
+
+    
+    // useEffect(() => {
+    //   fetch({url}+"/generate_profile"), {
+    //     method: 'POST',
+    //     headers:{
+    //         'guidance': 'content_string',
+    //     },
+    //     body: JSON.stringify("content")
+    //   }
+    // }, []
+    // );
 
     return (
         <div>
             <meta name="viewport" content="initial-scale=1, width=device-width" />
             <Box display="flex" justifyItems="center" padding="12px">
-                    <TextField id="outlined-basic" label="Hello World" variant="outlined" fullWidth="True"/>
-
-
-                <Button variant="contained" onClick={profileNav} className="btn btn-secondary me-2" style={{ marginLeft: '5px' }}>
-                Generate
+                <TextField id="outlined-basic" label="Hello World" variant="outlined" fullWidth={true} onChange={handleChange}/>
+                <Button variant="contained" onClick={onSubmit} className="btn btn-secondary me-2" style={{ marginLeft: '5px' }}>
+                    Generate
                 </Button>
             </Box>
 
