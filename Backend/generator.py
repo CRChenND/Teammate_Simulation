ac127ec1a5fde0db2b56588b665ec2618bf8c228
Gracellgg <453447505@qq.com>
2024-04-25 22:19:56 -0400

    commit

--- Backend/generator.py
+++ Backend/generator.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate, FewShotPromptTemplate
+from langchain_community.utilities.dalle_image_generator import DallEAPIWrapper
+
 from langchain_openai import ChatOpenAI
 from pydantic import BaseModel, Field
 from langchain.output_parsers import PydanticOutputParser
@@ -42,12 +44,20 @@ class Personality_traits_bool(BaseModel):
     Secure: bool
     Confident: bool
 
+# class Personality_traits_dict(BaseModel):
+#     Openness: str = Field(None, description="select from Curious, Inventive, Conservative, Cautious")
+#     Conscientiousness: str = Field(None, description="select from Efficient, Organized, Easygoing, Careless")
+#     Extraversion: str = Field(None, description="select from Outgoing, Energetic, Solitary, Reserved")
+#     Agreeableness: str = Field(None, description="select from Friendly, Compassionate, Challenging, Detached")
+#     Neuroticism: str = Field(None, description="select from Sensitive, Nervous, Secure, Confident")
+
 class Personality_traits_dict(BaseModel):
-    Openness: str = Field(None, description="select from Curious, Inventive, Conservative, Cautious")
-    Conscientiousness: str = Field(None, description="select from Efficient, Organized, Easygoing, Careless")
-    Extraversion: str = Field(None, description="select from Outgoing, Energetic, Solitary, Reserved")
-    Agreeableness: str = Field(None, description="select from Friendly, Compassionate, Challenging, Detached")
-    Neuroticism: str = Field(None, description="select from Sensitive, Nervous, Secure, Confident")
+    openness: str = Field(None, description="a value between 0 to 100 to show the openness, where 0 is close and 100 is open")
+    conscientiousness: str = Field(None, description="a value between 0 to 100 to show the conscientiousness, where 0 is spontaneous and 100 is conscientious")
+    extraversion: str = Field(None, description="a value between 0 to 100 to show the extraversion, where 0 is introverted and 100 is extraverted")
+    agreeableness: str = Field(None, description="a value between 0 to 100 to show the agreeableness, where 0 is hostile and 100 is agreeable")
+    neuroticism: str = Field(None, description="a value between 0 to 100 to show the neuroticism, where 0 is stable and 100 is neurotic")
+
 
 class Leadership_traits_bool(BaseModel):
     Coercive: bool
@@ -59,15 +69,15 @@ class Leadership_traits_bool(BaseModel):
 
 
 class Leadership_traits(str, Enum):
-    Coercive = 'Coercive'
-    Authoritative = 'Authoritative'
-    Affiliative = 'Affiliative'
-    Democratic = 'Democratic'
-    Pacesetting = 'Pacesetting'
-    Coaching = 'Coaching'
+    Coercive = 'coercive'
+    Authoritative = 'authoritative'
+    Affiliative = 'affiliative'
+    Democratic = 'democratic'
+    Pacesetting = 'pacesetting'
+    Coaching = 'coaching'
 
 class Leadership_traits_dict(BaseModel):
-    Leadership_style: str = Field(None, description="select from Coercive, Authoritative, Affiliative, Democratic, Pacesetting, Coaching")
+    Leadership_style: str = Field(None, description="select from coercive, authoritative, affiliative, democratic, pacesetting, coaching")
 
 class PersonaProfile(BaseModel):
     first_name: str = Field(None, description="First name of the persona, cannot be left blank, do not always use the most common names")
@@ -76,10 +86,11 @@ class PersonaProfile(BaseModel):
     gender: str = Field(None, description="Gender of the persona, cannot be left blank")
     english_proficiency: str = Field(None, description="English proficiency of the persona, e.g. beginner, intermediate, advanced, native")
     major: str = Field(None, description="Major of the persona, e.g. computer science, physics, etc., do not always use the most common majors")
-    grades: str = Field(None, description="Grades in the format freshman, sophomore, junior, senior")
+    grade: str = Field(None, description="Grades in the format freshman, sophomore, junior, senior")
     personality_traits: Personality_traits_dict
     leadership_traits: Leadership_traits #= Field(None, description="Leadership style of the persona: Coercive, Authoritative, Affiliative, Democratic, Pacesetting, Coaching")
     additional_info: str = Field(None, description="Any additional information about the persona, e.g. hobbies, interests, etc.")
+    profile: str = Field(None, description="A paragraph of persona description")
 
 
 
@@ -105,10 +116,26 @@ def generate_persona_attributes(input_data: PersonaInput) -> PersonaProfile:
     result = chain.invoke(input={
         'guidance':input_data
     })
-    print(result["text"])
-    return result["text"]
+    return eval(result["text"])
 
 
+def generate_image(input_str):
+    prompt = PromptTemplate(
+        template="Generate a realistic portrait of a college student based on {guidance}. This photo should be similar to a ID photo and will be used as a LinkedIn profile picture.",
+        input_variables=['guidance'],
+    )
+
+    chain = LLMChain(llm=ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.8),
+                     prompt=prompt)
+
+    full_prompt = chain.run(input_str)
+    if len(full_prompt) > 1000:
+        full_prompt = full_prompt[:1000]
+
+    image_url = DallEAPIWrapper().run(full_prompt)
+    print('image url')
+    print(image_url)
+    return image_url
 
 def generate_persona_description(input_data: PersonaInput) -> str:
     parser = PydanticOutputParser(pydantic_object=PersonaInput)
@@ -162,9 +189,11 @@ def post_process_output(output):
     output = output.replace("}", "")
     return output
 
+input = 'a 20-year-old Asian girl'
+generate_image(input)
 
 # load json file
-prompt = load_json('prompt.json')
+#prompt = load_json('prompt.json')
 
 #attributes = generate_persona_attributes({"guidance": prompt["random_attributes"]})
 
--- Backend/test_simulation.py
+++ Backend/test_simulation.py
@@ -1,5 +1,11 @@
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, PromptTemplate
 from langchain_openai.chat_models import ChatOpenAI
+from langchain_openai import OpenAI
+from langchain.chains import LLMChain
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.utilities.dalle_image_generator import DallEAPIWrapper
 import configparser
 import os
 from langchain.output_parsers import PydanticOutputParser
@@ -16,9 +22,225 @@ config.read(os.path.join(current_dir_path, 'config.ini'))
 os.environ['OPENAI_API_KEY'] = config['DEFAULT']['OPENAI_API_KEY']
 
 
-class output_format(BaseModel):
-    name: str
-    conversation: str
+# class output_format(BaseModel):
+#     name: str
+#     conversation: str
+#
+# #agent1 = generate_persona_attributes({"guidance": "Alice: a 18-year-old computer science student who is passionate about AI."})
+# agent1 = generate_persona_attributes({"guidance": ""})
+# agent1 = generate_persona_description({"guidance": prompt_json["profile_description"] + str(agent1)})
+# #agent2 = generate_persona_attributes({"guidance": "Bob: a 20-year-old applied physic student who is interested in investing in AI."})
+# agent2 = generate_persona_attributes({"guidance": ""})
+# agent2 = generate_persona_description({"guidance": prompt_json["profile_description"] + str(agent2)})
+# #agent3 = generate_persona_attributes({"guidance": "Charlie: a 22-year-old AI researcher who is working on a new AI model"})
+# agent3 = generate_persona_attributes({"guidance": ""})
+# agent3 = generate_persona_description({"guidance": prompt_json["profile_description"] + str(agent3)})
+#
+#
+# prompt = prompt_json["grouping_simulation"]
+# prompt += f"1. {agent1} 2. {agent2} 3. {agent3}"
+#
+# prompt = post_process_output(prompt)
+# print('prompt:', prompt)
+#
+# model = ChatOpenAI()
+# prompt = ChatPromptTemplate.from_messages(
+#     [
+#         (
+#             "system",
+#             prompt,
+#         ),
+#         MessagesPlaceholder(variable_name="history"),
+#         ("human", "{input}"),
+#     ]
+# )
+#
+#
+# runnable = prompt | model
+#
+#
+#
+# store = {}
+#
+#
+# def get_session_history(session_id: str) -> BaseChatMessageHistory:
+#     if session_id not in store:
+#         store[session_id] = ChatMessageHistory()
+#     return store[session_id]
+#
+#
+# with_message_history = RunnableWithMessageHistory(
+#     runnable,
+#     get_session_history,
+#     input_messages_key="input",
+#     history_messages_key="history",
+# )
+#
+# topic_example = 'Implement a library system program.'
+#
+#
+# # generate a topic using the prompt
+# topic_random = generate_topic({"guidance": topic_example})
+# topic_random = post_process_output(topic_random)
+#
+# print('topic:', topic_random)
+#
+# with_message_history.invoke(
+#     {"input": "Here is a project: " + topic_random + "Now these prople should form a group. Discuss how to finish this project in a group"},
+#     config={"configurable": {"session_id": "abc123"}},
+# )
+#
+# # print(with_message_history.get_session_history("abc123"))
+#
+# # Remembers
+# with_message_history.invoke(
+#     {"input": "please generate more conversation between them, you can stimulate some conflicts between them."},
+#     config={"configurable": {"session_id": "abc123"}},
+# )
+#
+# #print(with_message_history.get_session_history("abc123"))
+# text = str(with_message_history.get_session_history("abc123"))
+# print('//////////////////////////')
+# print(text)
+#
+# # Split the conversation into individual dialogues
+# dialogues = text.split('\n')
+#
+# # Create a list to hold the structured data
+# structured_data = []
+#
+# # Parse each dialogue and extract the speaker and their message
+# for idx, dialogue in enumerate(dialogues):
+#     if ': ' in dialogue:
+#         if idx == 0:
+#             pass
+#         else:
+#             speaker, message = dialogue.split(': ', 1)
+#             if speaker == 'Human':
+#                 pass
+#             elif speaker == 'AI':
+#                 speaker, message = message.split(': ', 1)
+#                 structured_data.append({'speaker': speaker, 'message': message})
+#             else:
+#                 structured_data.append({'speaker': speaker, 'message': message})
+#
+# # Convert the structured data into JSON format
+# json_data = json.dumps(structured_data, indent=4)
+#
+# # Save the JSON data to a file
+# with open('conversation.json', 'w') as json_file:
+#     json_file.write(json_data)
+
+store = {}
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+def convert_text_to_json(text_str):
+    # Split the conversation into individual dialogues
+    dialogues = text_str.split('\n')
+
+    # Create a list to hold the structured data
+    structured_data = []
+
+    # Parse each dialogue and extract the speaker and their message
+    for idx, dialogue in enumerate(dialogues):
+        if ': ' in dialogue:
+            if idx == 0:
+                pass
+            else:
+                speaker, message = dialogue.split(': ', 1)
+                if speaker == 'Human':
+                    pass
+                elif speaker == 'AI':
+                    speaker, message = message.split(': ', 1)
+                    structured_data.append({'speaker': speaker, 'message': message})
+                else:
+                    structured_data.append({'speaker': speaker, 'message': message})
+
+    # Convert the structured data into JSON format
+    json_data = json.dumps(structured_data, indent=4)
+
+    # Save the JSON data to a file
+    with open('conversation.json', 'w') as json_file:
+        json_file.write(json_data)
+
+    return json_data
+
+
+
+def generate_conversation(input_profile_list, topic=''):
+    """
+    generate the conversation based on the input profile list
+    """
+
+    prompt_json = load_json('prompt.json')
+    prompt = prompt_json["grouping_simulation"]
+    for idx, profile in enumerate(input_profile_list):
+        prompt += str(idx+1) + '. ' + profile
+    prompt = post_process_output(prompt)
+    print('prompt:', prompt)
+
+    model = ChatOpenAI()
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                prompt,
+            ),
+            MessagesPlaceholder(variable_name="history"),
+            ("human", "{input}"),
+        ]
+    )
+    runnable = prompt | model
+
+    with_message_history = RunnableWithMessageHistory(
+        runnable,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="history",
+    )
+
+    # generate a topic using the prompt
+    topic_random = generate_topic({"guidance": topic})
+    topic_random = post_process_output(topic_random)
+
+    print('topic:', topic_random)
+
+    with_message_history.invoke(
+        {
+            "input": "Here is a project: " + topic_random + "Now these prople should form a group. Discuss how to finish this project in a group"},
+        config={"configurable": {"session_id": "abc123"}},
+    )
+
+    with_message_history.invoke(
+        {"input": "please generate more conversation between them, you can stimulate some conflicts between them."},
+        config={"configurable": {"session_id": "abc123"}},
+    )
+
+    text = str(with_message_history.get_session_history("abc123"))
+    print('//////////////////////////')
+    print(text)
+
+    json_file = convert_text_to_json(text)
+
+    return json_file, with_message_history
+
+# def generate_more_conversation(with_message_history):
+#     text_previous = str(with_message_history.get_session_history("abc123"))
+#
+#     with_message_history.invoke(
+#         {"input": "please generate more conversation between them, you can stimulate some conflicts between them."},
+#         config={"configurable": {"session_id": "abc123"}},
+#     )
+#     text_after = str(with_message_history.get_session_history("abc123"))
+#     text = text_after.replace(text_previous, "", 1)
+#
+#     json_file = convert_text_to_json(text)
+#
+#     return json_file, with_message_history
+
 
 #agent1 = generate_persona_attributes({"guidance": "Alice: a 18-year-old computer science student who is passionate about AI."})
 agent1 = generate_persona_attributes({"guidance": ""})
@@ -30,100 +252,17 @@ agent2 = generate_persona_description({"guidance": prompt_json["profile_descript
 agent3 = generate_persona_attributes({"guidance": ""})
 agent3 = generate_persona_description({"guidance": prompt_json["profile_description"] + str(agent3)})
 
+agent_list = []
+agent_list.append(agent1)
+agent_list.append(agent2)
+agent_list.append(agent3)
 
-prompt = prompt_json["grouping_simulation"]
-prompt += f"1. {agent1} 2. {agent2} 3. {agent3}"
-
-prompt = post_process_output(prompt)
-print('prompt:', prompt)
-
-model = ChatOpenAI()
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            prompt,
-        ),
-        MessagesPlaceholder(variable_name="history"),
-        ("human", "{input}"),
-    ]
-)
+generate_conversation(agent_list)
 
 
-runnable = prompt | model
-
-from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain_core.chat_history import BaseChatMessageHistory
-from langchain_core.runnables.history import RunnableWithMessageHistory
-
-store = {}
 
 
-def get_session_history(session_id: str) -> BaseChatMessageHistory:
-    if session_id not in store:
-        store[session_id] = ChatMessageHistory()
-    return store[session_id]
 
 
-with_message_history = RunnableWithMessageHistory(
-    runnable,
-    get_session_history,
-    input_messages_key="input",
-    history_messages_key="history",
-)
 
-topic_example = 'Implement a library system program.'
-
-
-# generate a topic using the prompt
-topic_random = generate_topic({"guidance": topic_example})
-topic_random = post_process_output(topic_random)
-
-print('topic:', topic_random)
-
-with_message_history.invoke(
-    {"input": "Here is a project: " + topic_random + "Now these prople should form a group. Discuss how to finish this project in a group"},
-    config={"configurable": {"session_id": "abc123"}},
-)
-
-# print(with_message_history.get_session_history("abc123"))
-
-# Remembers
-with_message_history.invoke(
-    {"input": "please generate more conversation between them, you can stimulate some conflicts between them."},
-    config={"configurable": {"session_id": "abc123"}},
-)
-
-#print(with_message_history.get_session_history("abc123"))
-text = str(with_message_history.get_session_history("abc123"))
-print('//////////////////////////')
-print(text)
-
-# Split the conversation into individual dialogues
-dialogues = text.split('\n')
-
-# Create a list to hold the structured data
-structured_data = []
-
-# Parse each dialogue and extract the speaker and their message
-for idx, dialogue in enumerate(dialogues):
-    if ': ' in dialogue:
-        if idx == 0:
-            pass
-        else:
-            speaker, message = dialogue.split(': ', 1)
-            if speaker == 'Human':
-                pass
-            elif speaker == 'AI':
-                speaker, message = message.split(': ', 1)
-                structured_data.append({'speaker': speaker, 'message': message})
-            else:
-                structured_data.append({'speaker': speaker, 'message': message})
-
-# Convert the structured data into JSON format
-json_data = json.dumps(structured_data, indent=4)
-
-# Save the JSON data to a file
-with open('conversation.json', 'w') as json_file:
-    json_file.write(json_data)
 
